6d76ea26b738ca69f38ee80c66b926916ada864c
Isaac Souza <isaacsouza17@gmail.com>
2025-03-16 14:54:31 -0300

    Fetch whole card object

--- card-back-section.js
+++ card-back-section.js
@@ -1,53 +1,45 @@
 var trello = TrelloPowerUp.iframe()
 
-trello.board('id', 'name')
-    .then((board) => {
-        return trello.card('id', 'name', 'url')
-            .then((card) => {
-                return trello.get('board', 'shared', 'branchPrefix')
-                    .then(function (branchPrefix) {
-                        trello.card("all")
-                            .then(function (card) {
-                                console.log('card all', JSON.stringify(card, null, 2))
-                            })
+trello.card('all')
+    .then((card) => {
+        return trello.get('board', 'shared', 'branchPrefix')
+            .then(function (branchPrefix) {
+                // --------------------------------
+                // Branch name, ex: eng-123/johndoe/feature/custom-fields-v1
+                // --------------------------------
+                let branchName = 'Unknown'
 
-                        // --------------------------------
-                        // Branch name, ex: eng-123/custom-fields-v1
-                        // --------------------------------
-                        let branchName = 'Unknown'
+                if (typeof branchPrefix !== 'string') {
+                    branchPrefix = 'Undefined'
+                }
 
-                        if (typeof branchPrefix !== 'string') {
-                            branchPrefix = 'Undefined'
-                        }
+                try {
+                    const cardNumber = card.idShort
+                    const cardSlug = getCardSlugFromUrl(card.url)
+                    branchName = `${branchPrefix}-${cardNumber}/${cardSlug}`
+                } catch (error) {
+                    branchName = 'Error'
+                    console.error(error)
+                }
 
-                        try {
-                            const cardNumber = getCardNumberFromUrl(card.url)
-                            const cardSlug = getCardSlugFromUrl(card.url)
-                            branchName = `${branchPrefix}-${cardNumber}/${cardSlug}`
-                        } catch (error) {
-                            branchName = 'Error'
-                            console.error(error)
-                        }
+                window.branchNameInput.value = branchName
 
-                        window.branchNameInput.value = branchName
+                // --------------------------------
+                // Task code, ex: ENG-123
+                // --------------------------------
 
-                        // --------------------------------
-                        // Task code, ex: ENG-123
-                        // --------------------------------
+                let taskCode = 'Unknown'
 
-                        let taskCode = 'Unknown'
+                try {
+                    const cardNumber = card.idShort
+                    taskCode = `${branchPrefix}-${cardNumber}`
+                    taskCode = taskCode.toUpperCase()
+                } catch (error) {
+                    taskCode = 'Error'
+                    console.error(error)
+                }
 
-                        try {
-                            const cardNumber = getCardNumberFromUrl(card.url)
-                            taskCode = `${branchPrefix}-${cardNumber}`
-                            taskCode = taskCode.toUpperCase()
-                        } catch (error) {
-                            taskCode = 'Error'
-                            console.error(error)
-                        }
-
-                        window.taskCodeInput.value = taskCode
-                    })
+                window.taskCodeInput.value = taskCode
             })
     })
 
