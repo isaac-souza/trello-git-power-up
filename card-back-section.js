1289f1a71d8e5b7a983dec753c8ce25c0a35e018
Isaac Souza <isaacsouza17@gmail.com>
2025-03-16 15:23:17 -0300

    Drop extra branch name piece, keep it simple stupid

--- card-back-section.js
+++ card-back-section.js
@@ -5,7 +5,7 @@ trello.card('all')
         return trello.get('board', 'shared', 'branchPrefix')
             .then(function (branchPrefix) {
                 // --------------------------------
-                // Branch name, ex: eng-123/johndoe/feature/custom-fields-v1
+                // Branch name, ex: eng-123/custom-fields-v1
                 // --------------------------------
                 let branchName = 'Unknown'
 
@@ -13,29 +13,10 @@ trello.card('all')
                     branchPrefix = 'Undefined'
                 }
 
-                let member = null
-
-                try {
-                    const memberFullName = card.members.length > 0
-                        ? slug(card.members?.at(0)?.fullName ?? null)
-                        : null
-
-                    member = (typeof memberFullName === 'string' && memberFullName.length > 0)
-                        ? `${memberFullName}`
-                        : ''
-
-                    member = member.length > 0
-                        ? `${slug(member)}/`
-                        : ''
-                } catch (error) {
-                    console.error({ error, card })
-                    member = ''
-                }
-
                 try {
                     const cardNumber = card.idShort
                     const cardSlug = getCardSlugFromUrl(card.url)
-                    branchName = `${member}${branchPrefix}-${cardNumber}/${cardSlug}`
+                    branchName = `${branchPrefix}-${cardNumber}/${cardSlug}`
                 } catch (error) {
                     branchName = 'Error'
                     console.error(error)
